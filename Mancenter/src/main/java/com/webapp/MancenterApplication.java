2c87569391cd8030d1d94fdd143ec7f618528e08
lcalderon94 <luis_1994_2010@hotmail.com>
2021-11-14 15:32:16 +0100

    test

---
+++ Mancenter/src/main/java/com/webapp/MancenterApplication.java
@@ -0,0 +1,13 @@
+package com.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MancenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MancenterApplication.class, args);
+	}
+
+}
